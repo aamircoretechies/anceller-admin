32b3117fcc90272e6657ace3d959155dec93f6ad
Sohil Khan <sohil0021khan@gmail.com>
2025-12-22 19:24:47 +0530

    side arrow remove

--- src/layouts/demo1/sidebar/SidebarHeader.tsx
+++ src/layouts/demo1/sidebar/SidebarHeader.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { useDemo1Layout } from '../';
 import { toAbsoluteUrl } from '@/utils';
-import { SidebarToggle } from './';
+
 
 const SidebarHeader = forwardRef<HTMLDivElement, any>((props, ref) => {
   const { layout } = useDemo1Layout();
@@ -51,7 +51,7 @@ const SidebarHeader = forwardRef<HTMLDivElement, any>((props, ref) => {
       className="sidebar-header hidden lg:flex items-center relative justify-between px-3 lg:px-6 shrink-0"
     >
       {layout.options.sidebar.theme === 'light' ? lightLogo() : darkLogo()}
-      <SidebarToggle />
+
     </div>
   );
 });
